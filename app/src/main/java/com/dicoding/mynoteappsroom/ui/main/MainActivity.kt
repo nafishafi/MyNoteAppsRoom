b00256db35855a3a1cac1246ed7f1bb55ee1c353
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-10-22 13:11:19 +0700

    add Code

---
+++ app/src/main/java/com/dicoding/mynoteappsroom/ui/main/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.dicoding.mynoteappsroom.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.mynoteappsroom.R
+import com.dicoding.mynoteappsroom.ViewModelFactory
+import com.dicoding.mynoteappsroom.databinding.ActivityMainBinding
+import com.dicoding.mynoteappsroom.ui.insert.NoteAdapter
+import com.dicoding.mynoteappsroom.ui.insert.NoteAddUpdateActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+
+        adapter = NoteAdapter()
+
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+}
